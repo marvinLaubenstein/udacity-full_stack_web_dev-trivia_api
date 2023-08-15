ed00e0d75378b07e5445302f7addb5bd9b75e1ca
Marvin_Laubenstein <82942834+marvinLaubenstein@users.noreply.github.com>
2023-08-15 23:16:10 +0200

    feat: adding environment variables

--- backend/models.py
+++ backend/models.py
@@ -2,8 +2,10 @@ import os
 from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
+from settings import DB_NAME, DB_USER, DB_PASSWORD
 
-database_path = "postgresql://postgres:Database++.@localhost:5432/trivia"
+database_name = DB_NAME
+database_path = 'postgresql://{}:{}@{}/{}'.format(DB_USER,DB_PASSWORD,'localhost:5432', database_name)
 
 db = SQLAlchemy()
 
