b1e1ad602c5eb4a85de3a94a7baaed0442591697
Marvin_Laubenstein <82942834+marvinLaubenstein@users.noreply.github.com>
2023-08-15 23:16:38 +0200

    feat: adding environment variables to tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -5,6 +5,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
 from models import setup_db, Question, Category
+from settings import DB_NAME, DB_USER, DB_PASSWORD
+
+database_name = DB_NAME
+database_path = 'postgresql://{}:{}@{}/{}'.format(DB_USER,DB_PASSWORD,'localhost:5432', database_name)
 
 
 class TriviaTestCase(unittest.TestCase):
@@ -14,8 +18,7 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        # self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = database_path
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
